1ca5d3e5ae2a5101561fe67891ec4cd5d0b58f4d
jacexavinbello@gmail.com <Xavin616>
2022-08-21 15:49:51 +0100

    Another one

--- bs.py
+++ bs.py
@@ -6,14 +6,15 @@ def get_images(query):
     if res.status_code == 200:
         soup = bs(res.content, 'html.parser')
         imgs = soup.select('.lozad')
-        #print(imgs)
         if len(imgs) > 0:
             for i in imgs:
                 try:
                     src = 'https://wallpapers.com' + i['src']
                 except Exception as e:
                     src = 'https://wallpapers.com' + i['data-src']
-                new_src = src.replace('thumb', 'high')
+                new_src = src.replace('thumb', 'file')
+                code = new_src[len(new_src)-21:len(new_src)-4]
+                new_src = new_src[:len(new_src)-21]+(code*2)+new_src[len(new_src)-4:]
                 yield {
                     "type": "photo",
                     "media": new_src
